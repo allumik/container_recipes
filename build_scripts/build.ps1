b9204efdd21d697fd29756c57149a5f6ca114ae3
allumik <alvinmeltsov@gmail.com>
2023-02-17 01:47:52 +0100

    Tested building on Win and Mac

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -5,20 +5,23 @@
 # Start-Job -Name "Docker+Singularity build" -FilePath "./build.ps1"
 
 #Ask for the admin password for WSL sudo and store it in variable
-$secpass = Read-Host "Your WSL admin password" -AsSecureString
+# $secpass = Read-Host "Your WSL admin password" -AsSecureString
+$projdir = $PWD.Path
 
 #Build the Docker base container
-Get-Content "..\dockerfiles\r-bioverse.Dockerfile" | docker build --progress=plain --tag "allumik/r-bioverse" - *>> ../build.log
+Get-Content "$projdir\dockerfiles\r-bioverse.Dockerfile" | docker build --progress=plain --tag "allumik/r-bioverse" - *>> "$projdir/build.log"
 
 #Push the container to DockerHub
 docker image push allumik/r-bioverse
 
-#Run Singularity/Apptainer in WSL in su and build the SIF file (note: "`" is for escaping PS piping or newlines)
-[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secpass)) | `
-  wsl sudo -S apptainer build --force r-bioverse-dev.sif r-bioverse-dev.def `| tee ../build.log
+# #Run Singularity/Apptainer in WSL in su and build the SIF file (note: "`" is for escaping PS piping or newlines)
+# [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secpass)) | `
+#   wsl sudo -S apptainer build --force r-bioverse-dev.sif r-bioverse-dev.def `| tee ../build.log
+
 
 #Or run it in a Docker container instead of WSL
-# docker run --mount type=bind,source=$curr_dirr,target=/home/appuser/app/ -it kaczmarj/apptainer
+Get-Content "$projdir/dockerfiles/sif-build.Dockerfile" | docker build --progress=plain --tag "allumik/sif-builder" - *>> "$projdir/build.log"
+docker run --privileged -v $projdir\:/app allumik/sif-builder build /app/r-bioverse-dev.sif /app/def_files/r-bioverse-dev.def *>> "$projdir/build.log"
 
 
 ## Aaand go get a cup of coffee ☕️!
